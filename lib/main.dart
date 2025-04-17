694632c037ee83dc03a05634b49b0ecbc53ce681
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-04-17 16:52:33 -0300

    mobile

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:challenge_inspire_logic/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home(), debugShowCheckedModeBanner: false));
+}
