f2e94063fce0ea71f223a8c041867e8941298a1d
GustavoDesk <rocha.gustavo@escolar.ifrn.edu.br>
2025-04-17 19:32:08 -0300

    quero comer pizza com erick

---
+++ Api/src/configs/acess.js
@@ -0,0 +1,8 @@
+const acessEnvs = {
+    userAcess: process.env.DB_USER,
+    hostAcess: process.env.DB_HOST,
+    passwd: process.env.DB_PASSWORD,
+    databaseName: process.env.DB_NAME,
+    portAcess: process.env.DB_PORT
+}
+module.exports = acessEnvs;
