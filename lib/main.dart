2cd6484aedc28ad574e53ae557ac7aad2b87a908
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-04-17 17:43:20 -0300

    adicionando novas pastas para facilitar a organização

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:challenge_inspire_logic/Home.dart';
+import 'package:challenge_inspire_logic/views/HomePage.dart';
+import 'package:challenge_inspire_logic/views/pages/Login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Login(), debugShowCheckedModeBanner: false));
 }
