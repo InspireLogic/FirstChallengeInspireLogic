575e2622803d9845c18cb4088b80778699af43dc
JhonataOvirowa <jhonataovirowa@gmail.com>
2025-04-18 02:07:53 -0300

    Feito o userModel com poo

---
+++ Api/src/models/UserModel.js
@@ -0,0 +1,39 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../configs/databaseConn');
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('user', 'admin'),
+    defaultValue: 'user'
+  }
+}, {
+  sequelize,
+  modelName: 'User',
+  tableName: 'users',
+  timestamps: true
+});
+
+module.exports = User;
