94087bb111d1f15f03beb26f221c8a65750f6f64
GustavoDesk <rocha.gustavo@escolar.ifrn.edu.br>
2025-04-17 21:23:26 -0300

    fix/ correction model conection using sequelize

---
+++ Api/src/configs/checkDataBaseConnection.js
@@ -0,0 +1,13 @@
+const handleDatabaseErro = require('./handleErro');
+const sequelize = require('./sequelizeConnectDB');
+async function checkDataBaseConnection(){
+    try{
+        
+        return await sequelize.authenticate();
+    }catch(error){
+        handleDatabaseErro(error);
+        throw error
+    }
+
+}
+module.exports = checkDataBaseConnection;
